31a8b87ced9c2136d055986be4e4017e82dacb22
Ruben Bravo <108179700+rubenbradel@users.noreply.github.com>
2023-07-25 19:06:35 +0200

    Create portScan.sh

---
+++ portScan.sh
@@ -0,0 +1,22 @@
+#portScan.sh
+#!/bin/bash
+
+#EXIT function if CTRL+C
+function ctrl_c(){
+  echo -e "\nEXIT\n"
+  tput cnorm; exit 1
+}
+
+#Ctrl+C
+trap ctrl_c INT
+
+#Ocultar el cursor
+tput civis
+
+#Port Scanner
+for port in $(seq 1 65535); do
+  (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "Port $port - OPEN" &
+done; wait
+
+#Recuperar el cursor
+tput cnorm
